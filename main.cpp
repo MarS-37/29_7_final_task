061d2dff1e0a2b202395abe84339c14b74cdbc3c
Mars-37 <master_sbo@mail.ru>
2024-07-09 22:50:11 +0300

    method insert

--- main.cpp
+++ main.cpp
@@ -18,15 +18,18 @@ public:
 
 	void PushBack(int data);
 	void PushFront(int data);
+	void Insert(int pos, int data);
 
 private:
 	// pointer the first node
 	Node* p_head;
+	int list_size;
 };
 
 List::List() : p_head(nullptr) {}
 List::~List() {}
 
+// down the list
 void List::PushBack(int data)
 {
 	if (p_head == nullptr) {
@@ -44,13 +47,39 @@ void List::PushBack(int data)
 		// add new node to the end list
 		current->p_next = new Node(data);
 	}
+
+	++list_size;
 }
 
+// top of the list
 void List::PushFront(int data)
 {
 	// create a node and tie
 	// it to the current head
 	p_head = new Node(data, p_head);
+	++list_size;
+}
+
+// in the specified position
+void List::Insert(int pos, int data)
+{
+	if (pos < 0 || pos > list_size) {
+		throw std::out_of_range("Index out of range");
+	}
+
+	if (pos == 0) {
+		PushFront(data);
+	}
+	else {
+		Node* current = p_head;
+		
+		for (int i = 0; i < pos - 1; ++i) {
+			current = current->p_next;
+		}
+
+		current->p_next = new Node(data, current->p_next);
+		++list_size;
+	}
 }
 
 
@@ -71,6 +100,13 @@ int main()
 	list.PushFront(9);
 	list.PushFront(10);
 	list.PushFront(11);
+
+	list.Insert(0, 12);
+	list.Insert(2, 13);
+	list.Insert(4, 14);
+	list.Insert(5, 15);
+	list.Insert(7, 16);
+	list.Insert(8, 17);
 
 
 	return 0;
