e2ab64a4aa620b491eb805177ecfadebaf3ba784
Mars-37 <master_sbo@mail.ru>
2024-07-09 22:02:38 +0300

    construct

--- main.cpp
+++ main.cpp
@@ -1,20 +1,32 @@
 #include <iostream>
 
 
-class Node
+struct Node
 {
-public:
-	
+	Node* p_next;
+	int data;
+
+	Node(int data = int(), Node* p_next = nullptr) : data(data), p_next(p_next) {}
 };
 
 
 class List
 {
+public:
+	List();
+	~List();
 
+private:
+	// pointer the first node
+	Node* ptr_head;
 };
+
+List::List() : ptr_head(nullptr) {}
 
 
 int main()
 {
 
 }
+
+
