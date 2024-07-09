fe7a6f2b6a1061a066d2dbc443db050c61ef6d6c
Mars-37 <master_sbo@mail.ru>
2024-07-09 22:16:04 +0300

    method pushback

--- main.cpp
+++ main.cpp
@@ -16,17 +16,48 @@ public:
 	List();
 	~List();
 
+	void PushBack(int data);
+
 private:
 	// pointer the first node
-	Node* ptr_head;
+	Node* p_head;
 };
 
-List::List() : ptr_head(nullptr) {}
+List::List() : p_head(nullptr) {}
 List::~List() {}
+
+void List::PushBack(int data)
+{
+	if (p_head == nullptr) {
+		// if the list is empty,
+		// create a new one
+		p_head = new Node(data);
+	}
+	else {
+		Node* current = p_head;
+
+		while (current->p_next != nullptr) {
+			current = current->p_next;
+		}
+
+		// add new node to the end list
+		current->p_next = new Node(data);
+	}
+}
 
 int main()
 {
+	List list;
 
+	list.PushBack(0);
+	list.PushBack(1);
+	list.PushBack(2);
+	list.PushBack(3);
+	list.PushBack(4);
+	list.PushBack(5);
+
+
+	return 0;
 }
 
 
